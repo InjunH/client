91bd4ceb4e1ebfca4e02807a056f5ef914ce7469
INJUN <ijhwang@pood.pet>
2022-06-04 18:49:30 +0900

    header 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import ToolBar from "./components/ToolBar";
 
 const App = () => {
-  return <h2>커피 내기</h2>;
+  return (
+    <div>
+      <ToolBar></ToolBar>
+      <h2>커피 내기</h2>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/ToolBar.js
@@ -0,0 +1,9 @@
+const ToolBar = () => {
+  return (
+    <div style={{ minHeight: 50, backgroundColor: "#000", color: "#fff" }}>
+      <span>toolbar</span>
+    </div>
+  );
+};
+
+export default ToolBar;
