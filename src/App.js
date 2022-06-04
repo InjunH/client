d166f813ffb7ab65668848ccf0f593a746f221ac
INJUN <ijhwang@pood.pet>
2022-06-04 23:15:27 +0900

    footer 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ToolBar from "./components/ToolBar";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import Main from "./pages/Main";
 import "./styles/main.css";
 const App = () => {
@@ -8,7 +8,7 @@ const App = () => {
     <div style={{ minWidth: 600, margin: "auto" }}>
       <ToolBar></ToolBar>
       <Main></Main>
-      {/* <Footer></Footer> */}
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div style={{ minHeight: 50, backgroundColor: "#000", color: "#fff" }}>
+      <span>Footer</span>
+    </div>
+  );
+};
+
+export default Footer;
